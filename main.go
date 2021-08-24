29b5cf066c5e6ffbdbe9420ead3e7a4919f982dd
Mikhail Mukhin <mikhail.mukhin@indusoft.ru>
2021-08-24 11:19:50 +0300

    Let specify hostname

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ func New_Node(name string, deps ...string) *Node {
 type Graph []*Node
 
 var graph Graph
-var caseid, measured, dbname *string
+var caseid, measured, dbname, host *string
 var db *sql.DB
 
 func handle_formula(sfname *string, settings *string) {
@@ -184,12 +184,13 @@ func resolve() (Graph, error) {
 
 func main() {
 	caseid = flag.String("caseid", "(select top 1 id from cases order by id desc)", "caseid")
+	host = flag.String("host", "localhost", "host")
 	dbname = flag.String("db", "mb4", "db")
 	measured = flag.String("measured", "MeasuredMass", "measured")
 	
 	flag.Parse()
 
-	connstring := "server=localhost;user id=;trusted_connection=true;database=" + *dbname + ";"
+	connstring := "server=" + *host + ";user id=;trusted_connection=true;database=" + *dbname + ";"
 
 	var err error
 
